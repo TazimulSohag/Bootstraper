6543a94b6dcf44472184ab2ee3d650b746d0c69a
TazimulSohag <tazimulsohag@gmail.com>
2021-03-29 05:55:00 +0600

    base structure

--- app/src/main/java/co/banglabs/bootstraper/ui/pdf/PdfFragment.java
+++ app/src/main/java/co/banglabs/bootstraper/ui/pdf/PdfFragment.java
@@ -1,27 +1,23 @@
-package co.banglabs.bootstraper.ui.home;
+package co.banglabs.bootstraper.ui.pdf;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import co.banglabs.bootstraper.R;
 
-public class HomeFragment extends Fragment {
+public class PdfFragment extends Fragment {
 
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_pdf, container, false);
 
         return view;
     }
